91798bfc3f39b5a150dfb30983e4d796c2e77e3f
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-04-26 21:16:58 +0300

    add solution

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 // Итерационный подсчет n-го числа Фибоначчи
 
+function fibIterat(num) {
+	let first = 0;
+	let second = 1;
+	let result = 1;
+	for (let i = 3; i <= num; i++) {
+		result = first + second;
+		first = second;
+		second = result;
+	}
+	return result;
+}
 
-
+console.log(fibIterat(15)); // 377
 
 // Рекурсивное вычисление n-го числа Фибоначчи
 
+function fibRec(num) {
+	if (num <= 1) return 0;
+	if (num === 2) return 1;
+	return fibRec(num - 1) + fibRec(num - 2);
+}
 
-
+console.log(fibRec(14)); // 233
